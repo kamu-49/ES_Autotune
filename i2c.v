aea80eed72239968cab4ef9d717811eb6c09dc0c
khaelaharrod <113210421+khaelaharrod@users.noreply.github.com>
2023-05-11 03:58:09 -0400

    Update i2c.v

--- i2c.v
+++ i2c.v
@@ -1,45 +1,6 @@
 // ============================================================================
 // Copyright (c) 2012 by Terasic Technologies Inc.
 // ============================================================================
-//
-// Permission:
-//
-//   Terasic grants permission to use and modify this code for use
-//   in synthesis for all Terasic Development Boards and Altrea Development 
-//   Kits made by Terasic.  Other use of this code, including the selling 
-//   ,duplication, or modification of any portion is strictly prohibited.
-//
-// Disclaimer:
-//
-//   This VHDL or Verilog source code is intended as a design reference
-//   which illustrates how these types of functions can be implemented.
-//   It is the user's responsibility to verify their design for
-//   consistency and functionality through the use of formal
-//   verification methods.  Terasic provides no warranty regarding the use 
-//   or functionality of this code.
-//
-// ============================================================================
-//           
-//  Terasic Technologies Inc
-//  9F., No.176, Sec.2, Gongdao 5th Rd, East Dist, Hsinchu City, 30070. Taiwan
-//
-//
-//
-//                     web: http://www.terasic.com/
-//                     email: support@terasic.com
-//
-// ============================================================================
-//
-// Major Functions:i2c controller
-//
-// ============================================================================
-//
-// Revision History :
-// ============================================================================
-//   Ver  :| Author              :| Mod. Date :| Changes Made:
-//   V1.0 :| Allen Wang          :| 03/25/10  :|      Initial Revision
-//   V3.0 :| Young               :| 01/05/10  :|  vision 12.1
-// ============================================================================
 module i2c (
 			 CLOCK,
 			 I2C_SCLK,		//I2C CLOCK
