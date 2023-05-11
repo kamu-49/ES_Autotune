8b62e05eabfddd9a686ad2c191724abd4896e36e
khaelaharrod <113210421+khaelaharrod@users.noreply.github.com>
2023-05-11 03:55:34 -0400

    Update DE1_SoC_i2sound.v

--- DE1_SoC_i2sound.v
+++ DE1_SoC_i2sound.v
@@ -1,39 +1,4 @@
-// ============================================================================
-// Copyright (c) 2013 by Terasic Technologies Inc.
-// ============================================================================
-//
-// Permission:
-//
-//   Terasic grants permission to use and modify this code for use
-//   in synthesis for all Terasic Development Boards and Altera Development 
-//   Kits made by Terasic.  Other use of this code, including the selling 
-//   ,duplication, or modification of any portion is strictly prohibited.
-//
-// Disclaimer:
-//
-//   This VHDL/Verilog or C/C++ source code is intended as a design reference
-//   which illustrates how these types of functions can be implemented.
-//   It is the user's responsibility to verify their design for
-//   consistency and functionality through the use of formal
-//   verification methods.  Terasic provides no warranty regarding the use 
-//   or functionality of this code.
-//
-// ============================================================================
-//           
-//  Terasic Technologies Inc
-//  9F., No.176, Sec.2, Gongdao 5th Rd, East Dist, Hsinchu City, 30070. Taiwan
-//  
-//  
-//                     web: http://www.terasic.com/  
-//                     email: support@terasic.com
-//
-// ============================================================================
-//Date:  Thu Jul 11 11:26:45 2013
-// ============================================================================
-
-//`define ENABLE_HPS
-
-
+// Modified by Khaela Harrod (klh2173)
 module DE1_SoC_i2sound(
 
       ///////// ADC /////////
